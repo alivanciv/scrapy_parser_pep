229d804ff52d7266512f5efd11b0d36c672f5181
Алексей Иванцив <alivanciv@gmail.com>
2024-04-18 23:50:54 +1100

    Sprint19. Scrapy. Final #1. DB disconnect

--- pep_parse/models.py
+++ pep_parse/models.py
@@ -1,34 +1,34 @@
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import declared_attr
+# from sqlalchemy import Column, Integer, String
+# from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.orm import declared_attr
 
-Base = declarative_base()
-
-
-class Pep(Base):
-
-    @declared_attr
-    def __tablename__(cls):
-        return cls.__name__.lower()
-
-    id = Column(Integer, primary_key=True)
-    number = Column(Integer)
-    name = Column(String(200))
-    status = Column(String(20))
-
-    def __repr__(self):
-        return f'{self.number} {self.name} ({self.status})'
+# Base = declarative_base()
 
 
-class Statistic(Base):
+# class Pep(Base):
 
-    @declared_attr
-    def __tablename__(cls):
-        return cls.__name__.lower()
+#     @declared_attr
+#     def __tablename__(cls):
+#         return cls.__name__.lower()
 
-    id = Column(Integer, primary_key=True)
-    status = Column(String(20))
-    status_count = Column(Integer)
+#     id = Column(Integer, primary_key=True)
+#     number = Column(Integer)
+#     name = Column(String(200))
+#     status = Column(String(20))
 
-    def __repr__(self):
-        return f'{self.status} - {self.status_count}'
+#     def __repr__(self):
+#         return f'{self.number} {self.name} ({self.status})'
+
+
+# class Statistic(Base):
+
+#     @declared_attr
+#     def __tablename__(cls):
+#         return cls.__name__.lower()
+
+#     id = Column(Integer, primary_key=True)
+#     status = Column(String(20))
+#     status_count = Column(Integer)
+
+#     def __repr__(self):
+#         return f'{self.status} - {self.status_count}'
